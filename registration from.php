950b3075f660bf78d8bcc6a60bc0a87653d1c130
Raviparmar7 <68985617+Raviparmar7@users.noreply.github.com>
2021-06-11 09:03:35 -0700

    Add files via upload

---
+++ registration from.php
@@ -0,0 +1,41 @@
+<html>
+ <body>
+ <center><h1>Registration From</h1>
+   <form action="getpost.php" method="POST">
+   <table>
+   <tr>
+   <td>Firstname</td>
+   <td bgcolor="yellow"><input type="text" name="txt1" required/></td>
+   </tr>
+
+   <tr>
+   <td>Lastname</td>
+   <td bgcolor="red"><input type="text" name="txt2" required/></td>
+   </tr>
+
+   <tr>
+   <td>Email</td>
+   <td bgcolor="pink"><input type="email" name="txt3" required/></td>
+   </tr>
+
+   <tr>
+   <td>password</td>
+   <td bgcolor="green"><input type="password" name="txt4" required/></td>
+   </tr>
+
+   <tr>
+   <td><input type="submit"/></td>
+   <td><input type="reset"/></td>
+   </tr>
+
+
+   
+   </table>
+
+  </form>
+
+
+
+
+ </body>
+</html>
